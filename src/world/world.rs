183a47c80253692413c86b8ff7d552cc007db09b
AcrylicShrimp <led789zxpp@naver.com>
2020-05-27 15:10:27 +0900

    REFACTOR: Drop the map creation feature.

--- src/world/world.rs
+++ src/world/world.rs
@@ -1,12 +1,10 @@
 extern crate sdl2;
 
-use super::gen::Gen;
 use super::map::Map;
-use super::map_generator::generate_map;
 use super::map_renderer::MapRenderer;
 
 pub struct World {
-    map: Map,
+    map: Option<Map>,
     map_renderer: MapRenderer,
 }
 
@@ -40,63 +38,24 @@ impl World {
             texture_creator,
         );
 
-        let mut gen = Gen::new();
-
-        gen.add_tile(0, 2f32);
-        gen.add_tile(1, 4f32);
-        gen.add_tile(2, 4f32);
-        gen.add_tile(3, 1f32);
-
-        let mut constraints = std::collections::HashMap::new();
-
-        constraints.insert(0, vec![0, 3]);
-        constraints.insert(1, vec![1, 2, 3]);
-        constraints.insert(2, vec![1, 2, 3]);
-        constraints.insert(3, vec![0, 1, 2, 3]);
-
-        let mut initials: Vec<Vec<bool>> = Vec::new();
-
-        for _ in 0..40 * 30 {
-            initials.push(vec![true; 4]);
-        }
-
-        for index in 0..40 * 30 {
-            let x = index % 40;
-            let y = index / 40;
-
-            if x == 0 || x == 39 || y == 0 || y == 29 {
-                initials[index][0] = false;
-                initials[index][1] = false;
-                initials[index][2] = false;
-                initials[index][3] = true;
-            }
-        }
-
-        initials[1 + 1 * 40][0] = true;
-        initials[1 + 1 * 40][1] = false;
-        initials[1 + 1 * 40][2] = false;
-        initials[1 + 1 * 40][3] = false;
-
-        initials[38 + 28 * 40][0] = true;
-        initials[38 + 28 * 40][1] = false;
-        initials[38 + 28 * 40][2] = false;
-        initials[38 + 28 * 40][3] = false;
-
         World {
-            map: Map::from_data(
-                40,
-                30,
-                gen.gen(40, 30, &constraints, Some(initials)).unwrap(),
-            ),
+            map: None,
             map_renderer,
         }
     }
 
-    pub fn map(&self) -> &Map {
+    pub fn map(&self) -> &Option<Map> {
         &self.map
     }
 
     pub fn render(&self, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>) {
-        self.map_renderer.render(canvas, &self.map);
+        match &self.map {
+            Some(map) => self.map_renderer.render(canvas, &map),
+            None => {}
+        }
+    }
+
+    pub fn init_map(&mut self, map: Map) {
+        self.map = Some(map);
     }
 }
