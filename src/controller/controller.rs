485775e23c6d3be83b0888eaeb7c3d6bd3314578
AcrylicShrimp <led789zxpp@naver.com>
2020-05-28 00:19:05 +0900

    FEAT: Add a packet sending ability.

--- src/controller/controller.rs
+++ src/controller/controller.rs
@@ -1,13 +1,15 @@
 use super::super::input::input::Input;
+use super::super::network::socket::Socket;
 use super::super::object::object::Object;
-use super::super::world::world::World;
+use super::super::world::map::Map;
 
 pub trait Controller {
     fn update(
         &mut self,
         now: std::time::Instant,
         input: &Input,
-        world: &World,
+        map: &Map,
         object: &mut Object,
+        socket: &mut Socket,
     );
 }
--- src/controller/player_controller.rs
+++ src/controller/player_controller.rs
@@ -1,12 +1,16 @@
 extern crate sdl2;
 
 use super::super::input::input::Input;
+use super::super::network::socket::Socket;
 use super::super::object::object::Object;
-use super::super::world::world::World;
+use super::super::world::map::Map;
 use super::controller::Controller;
+
+use byteorder::WriteBytesExt;
 
 pub struct PlayerController {
     delay: f32,
+    player_id: Option<u64>,
     last_move: Option<std::time::Instant>,
 }
 
@@ -14,8 +18,13 @@ impl PlayerController {
     pub fn new(delay: f32) -> PlayerController {
         PlayerController {
             delay,
+            player_id: None,
             last_move: None,
         }
+    }
+
+    pub fn set_player_id(&mut self, player_id: u64) {
+        self.player_id = Some(player_id);
     }
 }
 
@@ -24,40 +33,56 @@ impl Controller for PlayerController {
         &mut self,
         now: std::time::Instant,
         input: &Input,
-        world: &World,
+        map: &Map,
         object: &mut Object,
+        socket: &mut Socket,
     ) {
-        match world.map() {
-            Some(map) => {
+        match self.player_id {
+            Some(player_id) => {
                 if self.last_move.is_none() {
                     self.last_move = Some(now);
                     return;
                 }
+
                 if (now - self.last_move.unwrap()).as_secs_f32() < self.delay {
                     return;
                 }
+
+                let mut packet = vec![];
+
+                packet.write_u16::<byteorder::LittleEndian>(2).unwrap();
+
                 if input.up() != input.down() {
                     if input.up() {
                         if map.get_block(object.x as u32, object.y as u32 - 1) == 0 {
-                            object.y -= 1;
+                            packet.push(0);
+                            socket.send(packet);
                             self.last_move = Some(now);
                         }
                     } else {
                         if map.get_block(object.x as u32, object.y as u32 + 1) == 0 {
-                            object.y += 1;
+                            packet.push(1);
+                            socket.send(packet);
                             self.last_move = Some(now);
                         }
                     }
                 }
+
+                packet = vec![];
+
+                packet.write_u16::<byteorder::LittleEndian>(2).unwrap();
+
                 if input.left() != input.right() {
                     if input.left() {
                         if map.get_block(object.x as u32 - 1, object.y as u32) == 0 {
-                            object.x -= 1;
+                            packet.push(2);
+                            socket.send(packet);
                             self.last_move = Some(now);
                         }
                     } else {
                         if map.get_block(object.x as u32 + 1, object.y as u32) == 0 {
-                            object.x += 1;
+                            packet.push(3);
+                            socket.send(packet);
                             self.last_move = Some(now);
                         }
                     }
