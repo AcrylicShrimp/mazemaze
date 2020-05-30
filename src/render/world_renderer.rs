0b7f310c022a149e7d5145a7d4585eded6abea27
AcrylicShrimp <led789zxpp@naver.com>
2020-05-30 19:40:20 +0900

    FEAT: Use a player renderer.

--- src/render/world_renderer.rs
+++ src/render/world_renderer.rs
@@ -2,18 +2,21 @@ extern crate sdl2;
 
 use super::super::world::world;
 use super::map_renderer;
+use super::player_renderer;
 use super::renderer;
 use sdl2::render;
 use sdl2::video;
 
 pub struct WorldRenderer {
 	map_renderer: map_renderer::MapRenderer,
+	player_renderer: player_renderer::PlayerRenderer,
 }
 
 impl WorldRenderer {
 	pub fn new(renderer: &renderer::Renderer) -> Result<WorldRenderer, String> {
 		Ok(WorldRenderer {
 			map_renderer: map_renderer::MapRenderer::new(renderer)?,
+			player_renderer: player_renderer::PlayerRenderer::new(renderer)?,
 		})
 	}
 
@@ -23,9 +26,11 @@ impl WorldRenderer {
 		canvas: &mut render::Canvas<video::Window>,
 	) -> Result<(), String> {
 		match world.map() {
-			Some(map) => self
-				.map_renderer
-				.render(map.width(), map.height(), map.data(), canvas)?,
+			Some(map) => {
+				self.map_renderer
+					.render(map.width(), map.height(), map.data(), canvas)?;
+				self.player_renderer.render(world.players(), canvas)?;
+			}
 			None => {}
 		}
 
